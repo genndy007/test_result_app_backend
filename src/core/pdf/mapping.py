7e28ec276e38247b6d3431ca57510c5c89035805
Hennadii Kochev <hennadii.kochev@ronis.info>
2023-05-30 02:01:25 +0300

    Add suite name for pdf report

---
+++ src/core/pdf/mapping.py
@@ -0,0 +1,6 @@
+STATUS_MAPPING = {
+    "success": "Successful",
+    "fail": "Failure",
+    "retest": "Re-test again",
+    "default": "Unknown",
+}
--- src/core/pdf/report.py
+++ src/core/pdf/report.py
@@ -1,11 +1,13 @@
 from fpdf import FPDF
 
 from src.core.pdf.common import FONT_FAMILY, Font, Color
+from src.core.pdf.mapping import STATUS_MAPPING
 
 
 class ReportPDF:
-    def __init__(self, file_name):
+    def __init__(self, file_name, data):
         self.file_name = file_name
+        self.data = data
         self.pdf = FPDF()
 
     def prepare(self):
@@ -22,11 +24,42 @@ class ReportPDF:
 
 
 class TestRunReportPDF(ReportPDF):
-    def build_heading(self):
+
+    def build_report_heading(self):
         self.pdf.set_font(*Font.REPORT_HEADING)
         self.pdf.set_fill_color(*Color.WHITE)
         self.pdf.cell(self.pdf.epw, Font.HEADING_FONT_SIZE, 'Test Run Report', align='C', fill=True)
         self.pdf.ln()
+        self.pdf.ln(5)
+
+    def build_title(self):
+        self.pdf.set_font(*Font.GENERAL_HEADING)
+        self.pdf.set_fill_color(*Color.WHITE)
+
+        row1 = {
+            'Test Suite': self.data['test_suite']['name'],
+            'Had been run at': self.data['timestamp'],
+        }
+        row2 = {
+            'About suite': self.data['test_suite']['description'],
+            'Result of Run': STATUS_MAPPING.get(self.data['result'], 'UNKNOWN'),
+        }
+        rows = [row1, row2]
+
+        for row in rows:
+            for key, value in row.items():
+                self.pdf.set_font(*Font.GENERAL_HEADING)
+                self.pdf.set_fill_color(*Color.HEADING)
+                self.pdf.cell(self.pdf.epw * 0.2, Font.GENERAL_FONT_SIZE, key, align='L', fill=True)
+
+                self.pdf.set_font(*Font.GENERAL_TEXT)
+                self.pdf.set_fill_color(*Color.WHITE)
+                self.pdf.cell(self.pdf.epw * 0.3, Font.GENERAL_FONT_SIZE, value, align='L', fill=True)
+
+            self.pdf.ln()
+
+
 
     def build_pdf(self):
-        self.build_heading()
+        self.build_report_heading()
+        self.build_title()
