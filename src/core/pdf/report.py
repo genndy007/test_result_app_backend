ce75864947b3b665cb38785bbe3080adf3aa7095
Hennadii Kochev <hennadii.kochev@ronis.info>
2023-05-30 01:10:42 +0300

    add heading for pdf

--- src/core/pdf/report.py
+++ src/core/pdf/report.py
@@ -1,19 +1,32 @@
 from fpdf import FPDF
 
+from src.core.pdf.common import FONT_FAMILY, Font, Color
 
-class TestRunReportPDF:
+
+class ReportPDF:
     def __init__(self, file_name):
         self.file_name = file_name
         self.pdf = FPDF()
 
     def prepare(self):
-        self.pdf.set_font("helvetica", "B", 16)
+        self.pdf.set_line_width(1 / 10 ** 5)
 
     def build_pdf(self):
-        self.pdf.cell(40, 10, "Hello World!")
+        pass
 
     def make(self):
         self.prepare()
         self.pdf.add_page()
         self.build_pdf()
         self.pdf.output(self.file_name)
+
+
+class TestRunReportPDF(ReportPDF):
+    def build_heading(self):
+        self.pdf.set_font(*Font.REPORT_HEADING)
+        self.pdf.set_fill_color(*Color.WHITE)
+        self.pdf.cell(self.pdf.epw, Font.HEADING_FONT_SIZE, 'Test Run Report', align='C', fill=True)
+        self.pdf.ln()
+
+    def build_pdf(self):
+        self.build_heading()
