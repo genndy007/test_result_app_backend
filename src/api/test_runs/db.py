658934bfaca0cff34ecb19a9c2e76fc55d5fc64a
Hennadii Kochev <hennadii.kochev@ronis.info>
2023-06-18 13:16:05 +0300

    refactor code to make db query in separate file

---
+++ src/api/test_runs/db.py
@@ -0,0 +1,48 @@
+from src.models import DBSession
+from src.models.test_case import TestCase, TestStep
+from src.models.test_suite import TestRun, TestCaseTestRun, TestSuite
+
+
+def get_test_run_by_id(test_run_id):
+    test_run = DBSession.query(TestRun).filter(TestRun.id == test_run_id).first()
+    if not test_run:
+        return {}
+
+    test_suite_qs = DBSession.query(TestSuite).filter(TestSuite.id == test_run.test_suite_id).first()
+    test_run_dict = {
+        'id': test_run.id,
+        'result': test_run.result,
+        'timestamp': test_run.created.strftime('%Y-%m-%d %H:%M:%S'),
+        'test_suite': {
+            'name': test_suite_qs.name,
+            'description': test_suite_qs.description,
+        },
+        'test_cases': [],
+    }
+
+    test_cases_ids_results = DBSession.query(TestCaseTestRun.test_case_id, TestCaseTestRun.status) \
+        .filter(TestCaseTestRun.test_run_id == test_run.id).all()
+    for test_case_id_result in test_cases_ids_results:
+        test_case_id, test_case_result = test_case_id_result
+        test_case_details = DBSession.query(TestCase).filter(TestCase.id == test_case_id).first()
+        test_case_dict = {
+            'id': test_case_id,
+            'name': test_case_details.name,
+            'description': test_case_details.description,
+            'status': test_case_result,
+            'precondition': test_case_details.precondition,
+            'postcondition': test_case_details.postcondition,
+            'test_steps': []
+        }
+
+        test_steps_qs = DBSession.query(TestStep).filter(TestStep.test_case_id == test_case_id).order_by(TestStep.order)
+        for test_step in test_steps_qs:
+            test_step_dict = {
+                'content': test_step.content,
+                'order': test_step.order,
+            }
+            test_case_dict['test_steps'].append(test_step_dict)
+
+        test_run_dict['test_cases'].append(test_case_dict)
+
+    return test_run_dict
--- src/api/test_runs/views.py
+++ src/api/test_runs/views.py
@@ -9,6 +9,7 @@ from src.models.user import User
 from src.models.test_case import TestCase, TestStep
 from src.models.test_suite import TestRun, TestCaseTestRun, TestSuite
 from src.utils import message_response
+from .db import get_test_run_by_id
 from . import test_runs
 
 
@@ -18,51 +19,21 @@ def list_my():
     if not user:
         return message_response('Authenticate at /auth/login first', HTTPStatus.FORBIDDEN)
 
-    test_suites_ids = [
-        row[0] for row in DBSession.query(TestSuite.id).filter(TestSuite.project_id == user.active_project_id).all()
-    ]
+    test_suites_qs = DBSession.query(TestSuite.id).filter(TestSuite.project_id == user.active_project_id)
+    test_suites_ids = [row[0] for row in test_suites_qs]
 
     test_runs_list = []
     test_runs_qs = DBSession.query(TestRun).filter(TestRun.test_suite_id.in_(test_suites_ids))
     for test_run in test_runs_qs:
-        test_suite_qs = DBSession.query(TestSuite).filter(TestSuite.id == test_run.test_suite_id).first()
-        test_run_dict = {
-            'id': test_run.id,
-            'result': test_run.result,
-            'timestamp': test_run.created.strftime('%Y-%m-%d %H:%M:%S'),
-            'test_suite': {
-                'name': test_suite_qs.name,
-                'description': test_suite_qs.description,
-            },
-            'test_cases': [],
-        }
-
-        test_cases_ids_results = DBSession.query(TestCaseTestRun.test_case_id, TestCaseTestRun.status)\
-            .filter(TestCaseTestRun.test_run_id == test_run.id).all()
-        for test_case_id_result in test_cases_ids_results:
-            test_case_id, test_case_result = test_case_id_result
-            test_case_details = DBSession.query(TestCase).filter(TestCase.id == test_case_id).first()
-            test_case_dict = {
-                'id': test_case_id,
-                'name': test_case_details.name,
-                'description': test_case_details.description,
-                'status': test_case_result,
-                'precondition': test_case_details.precondition,
-                'postcondition': test_case_details.postcondition,
-                'test_steps': []
-            }
-
-            test_steps_qs = DBSession.query(TestStep).filter(TestStep.test_case_id == test_case_id).order_by(TestStep.order)
-            for test_step in test_steps_qs:
-                test_step_dict = {
-                    'content': test_step.content,
-                    'order': test_step.order,
-                }
-                test_case_dict['test_steps'].append(test_step_dict)
-
-            test_run_dict['test_cases'].append(test_case_dict)
-
+        test_run_dict = get_test_run_by_id(test_run.id)
         test_runs_list.append(test_run_dict)
 
     return make_response({'test_runs': test_runs_list}, HTTPStatus.OK)
 
+
+
+@test_runs.route('/<int:test_run_id>/report', methods=['GET'])
+def id_report(test_run_id):
+    test_run_dict = get_test_run_by_id(test_run_id)
+
+    return make_response({'test_run': test_run_dict}, HTTPStatus.OK)
